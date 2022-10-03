c284b4103d18c3bdfd13d6061f5f7557e2589c7b
Celso Patiri <celsobenedetti2@gmail.com>
2022-10-03 10:11:10 -0300

    setup: (vue client) upload text file to server

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import axios from "axios";
+const file = ref();
+
+const handleFileUpload = async () => {
+    // debugger;
+    console.log("selected file", file?.value?.files[0]);
+    //Upload to server
+};
+const handleFileSubmit = async () => {
+    const formData = new FormData();
+    console.log({ upload: file.value });
+    formData.append("file", file.value.files[0]);
+    axios.post("http://localhost:3000/transactions/upload", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+};
+</script>
 
 <template>
-  <div><button class="btn btn-primary btn-sm">Teste</button></div>
+    <input ref="file" v-on:change="handleFileUpload()" type="file" />
+    <button class="btn btn-primary btn-md" @click="handleFileSubmit">Send</button>
 </template>
