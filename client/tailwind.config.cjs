84bf445b81744debf8cc11d1eb8d84f385345f89
Celso Patiri <celsobenedetti2@gmail.com>
2022-10-05 12:01:12 -0300

    new: tailwindcss setup

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,vue,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#41b883",
+                ["info-content"]: "#41b883",
+                ["primary-dark"]: "#34495e",
+            },
+            backgroundColor: {
+                ["tr-dark"]: "rgba(0,0,0,0.5)",
+            },
+        },
     },
     plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
